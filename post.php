4bfe49dcc10ffae4eab867319a5f21548a254ba9
Lev Giffune <levgiffune@accounts-MacBook-Air.local>
2018-09-30 11:09:38 -0400

    use "strlen()" instead of ".length" bc php got triggered

--- post.php
+++ post.php
@@ -23,7 +23,7 @@
 	$query = "CREATE TABLE IF NOT EXISTS posts(ID int NOT NULL AUTO_INCREMENT, loggedin BOOLEAN, name varchar(20), timestamph varchar(30), post TEXT, PRIMARY KEY (ID))";
 	mysqli_query($conn, $query);
 	if(isset($_POST['submit'])){
-		$loggedin = ($_COOKIE['token'].length < 20 ? 1 : 0)/* <-- see index.html line 34*/;
+		$loggedin = (strlen($_COOKIE['token']) < 20 ? 1 : 0)/* <-- see index.html line 34*/;
 		$token = $_COOKIE['token'];
 		$message = htmlspecialchars($_POST["message"], ENT_QUOTES);
 		$query = 'INSERT INTO posts (loggedin,name,timestamph,post) VALUES ('.$loggedin.',"anonyomous'.$token.'","'.$timestamp.'","'. $message.'")';
